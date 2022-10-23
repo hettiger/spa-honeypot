5d90873f910d102647f8e8269f001e1883843bbd
Martin Hettiger <martin@hettiger.com>
2022-10-23 17:29:48 +0200

    Make response helper more robust

--- src/Capabilities/RespondsWithNewFormToken.php
+++ src/Capabilities/RespondsWithNewFormToken.php
@@ -3,6 +3,7 @@
 namespace Hettiger\Honeypot\Capabilities;
 
 use Hettiger\Honeypot\FormToken;
+use Illuminate\Contracts\Support\Responsable;
 use Symfony\Component\HttpFoundation\Response;
 
 trait RespondsWithNewFormToken
@@ -11,6 +12,10 @@ trait RespondsWithNewFormToken
 
     protected function response(mixed $response, $withNewToken = true): Response
     {
+        if ($response instanceof Responsable) {
+            $response = $response->toResponse(request());
+        }
+
         if (! ($response instanceof Response)) {
             $response = response($response);
         }
