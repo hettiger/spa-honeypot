99dfc1ea47926f9270ec70febaee830c29ca87d3
Martin Hettiger <martin@hettiger.com>
2022-10-23 17:17:40 +0200

    Support providing a custom form token error response factory

--- src/Honeypot.php
+++ src/Honeypot.php
@@ -2,6 +2,7 @@
 
 namespace Hettiger\Honeypot;
 
+use Closure;
 use Hettiger\Honeypot\Capabilities\RecognizesGraphQLRequests;
 use Hettiger\Honeypot\Capabilities\RespondsWithNewFormToken;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,19 +12,35 @@ class Honeypot
     use RecognizesGraphQLRequests;
     use RespondsWithNewFormToken;
 
+    protected ?Closure $makeFormTokenErrorResponse = null;
+
     public function __construct(
         protected array $config,
     ) {
     }
 
+    public function respondToFormTokenErrorsUsing(Closure $makeResponse) {
+        $this->makeFormTokenErrorResponse = $makeResponse;
+    }
+
     public function formTokenErrorResponse($withNewToken = true)
     {
+        if ($this->makeFormTokenErrorResponse) {
+            return $this->response(
+                ($this->makeFormTokenErrorResponse)($this->isGraphQLRequest()),
+                $withNewToken
+            );
+        }
+
         if ($this->isGraphQLRequest()) {
-            return $this->response([
-                'errors' => [
-                    'message' => Response::$statusTexts[Response::HTTP_INTERNAL_SERVER_ERROR],
+            return $this->response(
+                [
+                    'errors' => [
+                        'message' => Response::$statusTexts[Response::HTTP_INTERNAL_SERVER_ERROR],
+                    ],
                 ],
-            ], $withNewToken);
+                $withNewToken
+            );
         }
 
         return Response::HTTP_INTERNAL_SERVER_ERROR;
--- tests/HoneypotTest.php
+++ tests/HoneypotTest.php
@@ -14,8 +14,7 @@ test('formTokenErrorResponse returns HTTP status code 500', function () {
 
 test(
     'formTokenErrorResponse returns GraphQL error response on GraphQL requests',
-    function (array $config
-    ) {
+    function (array $config) {
         withGraphQLRequest();
         $response = Honeypot::formTokenErrorResponse();
 
@@ -27,3 +26,31 @@ test(
             ->toBeTrue();
     })
 ->with('config');
+
+test(
+    'formTokenErrorResponse returns custom response when available',
+    function (array $config, bool $withNewToken, bool $isGraphQLRequest) {
+        $isGraphQLRequest ? withGraphQLRequest() : withRequest();
+        $expectedResponse = response('response fake');
+        $expectedGraphQLResponse = response('GraphQL response fake');
+
+        Honeypot::respondToFormTokenErrorsUsing(fn (bool $isGraphQLRequest) => $isGraphQLRequest
+            ? $expectedGraphQLResponse
+            : $expectedResponse
+        );
+
+        $actualResponse = Honeypot::formTokenErrorResponse($withNewToken);
+
+        expect($actualResponse)
+            ->toBe($isGraphQLRequest ? $expectedGraphQLResponse : $expectedResponse)
+            ->and($actualResponse->headers->contains($config['header'], Str::uuid()->toString()))
+            ->toBe($withNewToken);
+    }
+)
+->with('config')
+->with([
+    'new token' => [true, false],
+    'without token' => [false, false],
+    'new token, GraphQL' => [true, true],
+    'without token, GraphQL' => [false, true],
+]);
